d5690f008e225f22e118af18c4a9321a3745378f
pmbennett <paul@pmbennett.net>
2014-09-11 10:00:13 +0100

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name spendingAppApp
+ * @description
+ * # spendingAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('spendingAppApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spendingAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spendingAppApp
+ */
+angular.module('spendingAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
